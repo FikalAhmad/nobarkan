48f317cb37e6167106b9cac066ae1c9492e8e2ce
FikalAhmad <fikaalm26@gmail.com>
2023-11-02 22:05:08 +0700

    chore(next config): add env for use client

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    MOVIE_SEARCH: process.env.MOVIE_SEARCH,
+    API_MOVIE: process.env.API_MOVIE,
+    LINK_IMAGE: process.env.LINK_IMAGE,
+    BEARER_TOKEN: process.env.BEARER_TOKEN,
+    MOVIE_GENRE: process.env.MOVIE_GENRE,
+  },
   images: {
     remotePatterns: [
       {
